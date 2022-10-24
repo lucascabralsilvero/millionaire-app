dd6b9ee7fc2215b2b8631b1e20596a8f0c0ec179
Lucas Cabral <lucascabral6008@gmail.com>
2022-10-24 15:22:53 -0300

    adding trivia component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./app.css";
 import React, {useState} from "react";
+import Trivia from "./components/Trivia";
 
 function App() {
 
@@ -71,7 +72,14 @@ function App() {
 
   return (
     <div className="app">
-     <div className="main">main</div>
+     <div className="main">
+      <div className="top">
+        <div className="timer">30</div>
+      </div>
+      <div className="bottom">
+        <Trivia />
+      </div>
+     </div>
      <div className="pyramid">
       <ul className="moneyList">
         {
---
+++ src/components/Trivia.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Trivia = () => {
+  return (
+    <div className='trivia'>
+        <div className='question'>asdasdasd</div>
+        <div className='answers'>
+            <div className="answer">1</div>
+            <div className="answer">2</div>
+            <div className="answer">3</div>
+            <div className="answer">4</div>
+        </div>
+    </div>
+  )
+}
+
+export default Trivia
